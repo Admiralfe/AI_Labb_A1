94e02e5f9c4718023ed1837917390ffa10773012
Helmer Nylén <helmer@nylen.info>
2018-09-05 16:07:30 +0200

    Implementerat beta-pass, ändrat vad alpha-pass returnerar

--- hmm.cpp
+++ hmm.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <cmath>
 #include <iostream>
 
 #include "hmm.h"
@@ -11,7 +12,7 @@ using namespace std;
 typedef matrix mat;
 typedef vector<number> vec;
 
-vector<number> hmm::a_pass(const matrix& A, const matrix& B, const vector<number>& pi, const vector<int>& obs_seq, vector<number>& c) {
+matrix hmm::a_pass(const matrix& A, const matrix& B, const vector<number>& pi, const vector<int>& obs_seq, vector<number>& c) {
     int no_states = A.getHeight();
     int seq_length = obs_seq.size();
     
@@ -49,5 +50,52 @@ vector<number> hmm::a_pass(const matrix& A, const matrix& B, const vector<number
             alpha.set(i, t, alpha.get(i, t) * c[t]);
     }
     
-    return alpha.get_col(seq_length - 1);
+    return alpha;
+}
+
+//förutsätter att en alpha-pass redan gjorts och att värdena i c inte har ändrats
+vector<int> hmm::b_pass(const matrix& A, const matrix& B, const vector<number>& pi, const vector<int>& obs_seq, const vector<number>& c, const matrix& alpha) {
+    int no_states = A.getHeight();
+    int seq_length = obs_seq.size();
+    
+    assert(A.getWidth() == no_states);
+    assert(B.getHeight() == no_states);
+    assert(pi.size() == no_states);
+    assert(seq_length > 0);
+    assert(c.size() == seq_length);
+    assert(alpha.getHeight() == no_states);
+    assert(alpha.getWidth() == seq_length);
+
+    mat beta = mat(no_states, seq_length);
+
+    for (int i = 0; i < no_states; i++)
+        beta.set(i, seq_length - 1, c[no_states - 1]);
+    
+    for (int t = seq_length - 2; t >= 0; t--) {
+        for (int i = 0; i < no_states; i++) {
+            number sum = 0;
+            for (int j = 0; j < no_states; j++)
+                sum += A.get(i, j) * B.get(j, obs_seq[t + 1]) * beta.get(j, t + 1);
+            
+            beta.set(i, t, c[t] * sum);
+        }
+    }
+
+    int current_max;
+    int current;
+
+    vector<int> res = vector<int>(seq_length);
+
+    for (int t = 0; t < seq_length; t++) {
+        current_max = -1;
+        for (int i = 0; i < no_states; i++) {
+            current = alpha.get(i, t) * beta.get(i, t);
+            if (current > current_max) {
+                current_max = current;
+                res[t] = i;
+            }
+        }
+    }
+
+    return res;
 }
