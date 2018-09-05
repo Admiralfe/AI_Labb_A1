a883ef5f9a4a8173fe0726106a1b22a67f5b7b1b
Felix Liu <felixliu@kth.se>
2018-09-05 16:19:35 +0200

    Lagt till viterbi algoritm (ish)

--- hmm.cpp
+++ hmm.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <iostream>
+#include <cmath>
 
 #include "hmm.h"
 #include "globals.h"
@@ -14,7 +15,7 @@ typedef vector<number> vec;
 vector<number> hmm::a_pass(const matrix& A, const matrix& B, const vector<number>& pi, const vector<int>& obs_seq, vector<number>& c) {
     int no_states = A.getHeight();
     int seq_length = obs_seq.size();
-    
+
     assert(A.getWidth() == no_states);
     assert(B.getHeight() == no_states);
     assert(pi.size() == no_states);
@@ -48,6 +49,66 @@ vector<number> hmm::a_pass(const matrix& A, const matrix& B, const vector<number
         for (int i = 0; i < no_states; i++)
             alpha.set(i, t, alpha.get(i, t) * c[t]);
     }
-    
+
     return alpha.get_col(seq_length - 1);
+}
+
+vector<int> hmm::viterbi(matrix A, matrix B, vector<number> pi, vector<int> obs_seq) {
+    int no_states = A.getHeight();
+    int seq_length = obs_seq.size();
+
+    assert(A.getWidth() == no_states);
+    assert(B.getHeight() == no_states);
+    assert(pi.size() == no_states);
+    assert(seq_length > 0);
+
+    //Use logarithms for entries to avoid underflows
+    matrix log_delta = mat(no_states, seq_length);
+
+    //To keep track of most likely sequence of states.
+    vector<vector<int>> delta_index = vector<vector<int>>(no_states, vector<int>(seq_length));
+
+    //Initialize first column
+    for (int i = 0; i < no_states; i++) {
+        number entry = log(B.get(i, obs_seq[0])) + log(pi[i]);
+        log_delta.set(i, 0, entry);
+    }
+
+
+    int index_max;
+    //Loops over states
+    for (int i = 0; i < no_states; i++) {
+        //Loops over time steps
+        for (int t = 1; t < seq_length; t++) {
+
+            //Initialize the value of max for j = 0.
+            number max = log_delta.get(t - 1, 0) + log(A.get(0, i)) + log(B.get(i, obs_seq[0]));
+
+            //Find the max_{j} of all new deltas by iterating over j.
+            for (int j = 1; j < no_states; j++) {
+                number new_max = log_delta.get(t - 1, j) + log(A.get(j, i)) + log(B.get(i, obs_seq[j]));
+
+                if (new_max > max) {
+                    max = new_max;
+                    index_max = j;
+                }
+            }
+
+            log_delta.set(i, t, max);
+            delta_index[i][t] = index_max;
+        }
+    }
+
+    vector<int> res = vector<int>(seq_length);
+
+    //index_max should still have the relevant max value of the last time step since the for loop finished.
+    int max_state_index = index_max;
+    res[res.size() - 1] = max_state_index;
+
+    for (int t = res.size() - 2; t >= 0; t--) {
+        res[t] = delta_index[max_state_index][t];
+        max_state_index = delta_index[max_state_index][t];
+    }
+
+    return res;
 }
